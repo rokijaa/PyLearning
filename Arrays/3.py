2e92ee3d36c54f9bb4c2577354520f3011ea5aa8
rokija <rokijagames@gmail.com>
2024-07-20 21:34:57 +0200

    better reverse + print

--- Arrays/3.py
+++ Arrays/3.py
@@ -1,6 +1,8 @@
 # 3. Write a Python program to reverse the order of the items in the array.
 
-array = [1, 3, 5, 3, 7, 1, 9, 3]
+array = [1, 3, 5, 7, 9]
 
-array.reverse()
-print(array)
+newarray = str(array)
+revarray = array.reverse()
+
+print("The reverse of array " + newarray + " is " + str(array) )
